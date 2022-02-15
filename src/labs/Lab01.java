b0b65aaa185e0518f8fa6f3d8ef432fa5f8a6637
Bryn1andonly <89485008+Bryn1andonly@users.noreply.github.com>
2022-02-15 17:38:23 -0600

    Finished Lab
    :)

--- src/labs/Lab01.java
+++ src/labs/Lab01.java
@@ -1,6 +1,6 @@
 package labs;
 
-
+import java.util.Scanner;
 
 public class Lab01 {
 
@@ -17,7 +17,19 @@ public class Lab01 {
 	
 	public static void problem01() {
 		
+			//Scanner for user input
+			Scanner InKey = new Scanner(System.in);
 		
+			//Asks for user input
+			System.out.println("Enter a Positve integer");
+			int input1 = InKey.nextInt();
+			System.out.println("Enter another Positve integer");
+			int input2 = InKey.nextInt();
+			
+			//Multiplies the first input to the power of the second input
+			double power = Math.pow(input1, input2);
+			
+			System.out.println(input1+"^"+input2+"="+power);
 		
 	}
 	
@@ -26,6 +38,17 @@ public class Lab01 {
 	
 	public static void problem02() {
 		
+		//Scanner for user input
+		Scanner InKey = new Scanner(System.in);
+		
+		//Asks for user input
+		System.out.println("Enter a Positve integer");
+		int input1 = InKey.nextInt();
+		
+		//Grabs the square root of the number inputted
+		double root = Math.sqrt(input1);
+		
+		System.out.println("The square root of " + input1 + " is "+ root);
 		
 		
 	}
@@ -35,9 +58,20 @@ public class Lab01 {
 
 	public static void problem03() {
 		
+		//Scanner for user input
+		Scanner InKey = new Scanner(System.in);
 		
+		//Asks for user input on the sides of the triangle
+		System.out.println("Enter side A");
+		int input1 = InKey.nextInt();
+		System.out.println("Enter side B");
+		int input2 = InKey.nextInt();
 		
-
+		//Grabs the hypotenuse of the triangle
+		double hypot = Math.hypot(input1, input2);
+		
+		System.out.println("Hypotenuse = "+hypot);
+		
 	}
 
 
@@ -45,7 +79,37 @@ public class Lab01 {
 	
 	public static void problem04() {
 		
+		//Scanner for user input
+		Scanner InKey = new Scanner(System.in);
 		
+		//Initializes our variables
+		int max = Integer.MIN_VALUE;
+		int min = Integer.MAX_VALUE;
+		
+		System.out.println("Enter a Positve integer: ");
+		int input1 = InKey.nextInt();
+		
+		//Sets up loop for grabbing user input till a "0" is entered
+		while(input1 != 0) {
+			
+		
+			
+			System.out.println("Enter a Positve integer: ");
+			input1 = InKey.nextInt();
+			System.out.println(input1);
+			
+			max = Math.max(max, input1);
+			min = Math.min(min, input1);
+			
+			
+			
+			
+		}
+		
+			
+	
+			System.out.println("Greatest Number: " + max);
+			System.out.println("Lowest Number: " + min);
 		
 	}
 	
--- src/labs/Lab02.java
+++ src/labs/Lab02.java
@@ -1,6 +1,6 @@
 package labs;
 
-
+import java.util.Scanner;
 
 public class Lab02 {
 
@@ -17,7 +17,26 @@ public class Lab02 {
 	
 	public static void problem01() {
 		
+		//Scanner for user input
+		Scanner InKey = new Scanner(System.in);
 		
+		//Asks for user input on the range
+		System.out.println("Enter a Positve integer");
+		int input1 = InKey.nextInt();
+		System.out.println("Enter another Positve integer");
+		int input2 = InKey.nextInt();
+		
+		//sets up our Max and Min
+		int min = Math.min(input1, input2);
+		int max = Math.max(input2, input1);
+		
+		int scope = (max-min)+1;
+		
+		//Gets 10 numbers for the range
+		for(int i = 0; i < 10; i++) {
+			int rando = (int)(Math.random()*scope)+min;
+			System.out.println(rando);
+		}
 		
 		
 	}
@@ -27,7 +46,20 @@ public class Lab02 {
 	
 	public static void problem02() {
 		
+		//Scanner for user input
+		Scanner InKey = new Scanner(System.in);
 		
+		//Asks the user for the radius and the height of the cylinder
+		System.out.println("What is the radius of the cylinder?");
+		int radius = InKey.nextInt();
+		System.out.println("What is the height of the cylinder?");
+		int height = InKey.nextInt();
+		
+		
+		//Calculates the volume
+		double vol = Math.PI * Math.pow(radius, 2) * height;
+		
+		System.out.println("The volume of this cylinder is: " + vol);
 		
 	}
 
@@ -35,7 +67,25 @@ public class Lab02 {
 	
 
 	public static void problem03() {
-
+		
+		//Scanner for user input
+		Scanner InKey = new Scanner(System.in);
+		
+		
+		//Asks for user input on coordinate values
+		System.out.println("Enter coordinate 1's x value");
+		double x1 = InKey.nextInt();
+		System.out.println("Enter coordinate 1's y value");
+		double y1 = InKey.nextInt();	
+		System.out.println("Enter coordinate 2's x value");
+		double x2 = InKey.nextInt();
+		System.out.println("Enter coordinate 2's y value");
+		double y2 = InKey.nextInt();	
+		
+		//Math for finding the distance between the two points
+		double dist = Math.sqrt(Math.pow((x1-x2), 2) + Math.pow((y1-y2), 2));
+		
+		System.out.println("The distance between these two points is: " + dist);
 		
 		
 	}
@@ -45,8 +95,33 @@ public class Lab02 {
 	
 	public static void problem04() {
 		
+		//Scanner for user input
+		Scanner InKey = new Scanner(System.in);
 		
+		//Initializes our roots
+		double x1, x2;
 		
+		//Initializes our a,b, and c-values
+		double a, b, c;
+		
+		System.out.println("Enter the a-value");
+		a = InKey.nextInt();
+		System.out.println("Enter the b-value");
+		b = InKey.nextInt();	
+		System.out.println("Enter the c-value");
+		c = InKey.nextInt();
+		
+		//Discriminant
+		double d = Math.pow(b, 2) - 4 * a * c;
+		
+		//Math for quadratic formula
+		x1 = (-b + Math.pow(d, 0.5)) / (2 * a); 
+		x2 = (-b - Math.pow(d, 0.5)) / (2 * a);
+		
+		System.out.println("x1 = " + x1);
+		System.out.println("x2 = " + x2);
+		
+	
 		
 	}
 	
